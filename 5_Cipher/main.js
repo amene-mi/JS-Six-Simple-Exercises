aa893b2849bfc3b3f58ddf4539dccfb8f5ec977e
atefeh199170 <72982087+amene-mi@users.noreply.github.com>
2021-10-12 23:15:02 +0330

    solve question 5

---
+++ 5_Cipher/main.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById('show');
+const formEI = document.getElementById('form');
+const output = document.getElementById('output');
+
+const caesarCipher = ([...text], factor) => {
+    const arr = [...text];
+    let result = arr.map(function (item) {
+        return String.fromCharCode(item.charCodeAt(0) + factor);
+    });
+    const rgex = new RegExp(',', 'g');
+    output.innerText = result.toString().replace(rgex, '');
+};
+
+function validation(form) {
+    try {
+        if (form.elements[0].value && form.elements[1].value) {
+            if (isFinite(parseInt(form.elements[1].value))) {
+                caesarCipher(form.elements[0].value.split(""), parseInt(form.elements[1].value));
+            }
+            else { throw 'please input a Number..!'; }
+        }
+        else { throw 'Fields are required..!'; }
+    } catch (error) {
+        alert(error);
+    }
+}
+function onBtn(form = formEI) {
+    validation(form);
+}
+
+btn.addEventListener('click', () => onBtn());
